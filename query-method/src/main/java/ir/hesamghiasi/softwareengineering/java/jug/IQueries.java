bd839a40999c82d921184ce4068ca6458ddc50bf
hesamghiasi <ghiasihessam@yahoo.com>
2023-08-24 14:49:41 +0330

    remove compile error

--- query-method/src/main/java/ir/hesamghiasi/softwareengineering/java/jug/IQueries.java
+++ query-method/src/main/java/ir/hesamghiasi/softwareengineering/java/jug/IQueries.java
@@ -3,7 +3,7 @@ package ir.hesamghiasi.softwareengineering.java.jug;
 import java.util.List;
 
 public interface IQueries {
-    @HQL("from Book where title like :title order by title")
+//    @HQL("from Book where title like :title order by title")
     List<Book> findBooksByTitleWithPagination(String title);
 }
 
